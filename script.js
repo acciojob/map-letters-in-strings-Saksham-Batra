8a0ac7f5d220ee67e0a2d62aafe2b9fa30d89fce
Saksham-Batra <140792075+Saksham-Batra@users.noreply.github.com>
2024-04-18 15:40:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+function mapLetters(str) {
+	const letterMap = {};
+	for(let i = 0; i<str.length; i++){
+const letter = str[i];
+		if(!(letter in letterMap)) {
+letterMap[letter] = [];
+		}
+		letterMap[letter].push(i);
+	}
+	return letterMap;
+}
+
